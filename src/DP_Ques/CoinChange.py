72ef5d20d1e5a0e52c799bdd4443c9e11c61f677
ArcticRise <voxmichael@yahoo.com>
2022-03-27 23:57:03 -0400

    CoinChange.py

---
+++ src/DP_Ques/CoinChange.py
@@ -0,0 +1,11 @@
+def coinChange(coins,amount):
+    dp = [amount + 1] * (amount + 1)
+    dp[0] = 0
+
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], 1 + dp[i - coin])
+    return dp[amount] if dp[amount] != amount + 1 else -1
+
+print(coinChange([1,2,5],11))
